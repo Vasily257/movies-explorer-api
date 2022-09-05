33f7457d9947ab05f1cdd2c2afeceff26e5cc1aa
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-09-05 16:48:14 +0400

    feat: add user and movie models

---
+++ src/model/movie.js
@@ -0,0 +1,71 @@
+const { Schema, model } = require('mongoose');
+const validator = require('validator');
+
+const movieSchema = new Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(image) {
+        return validator.isURL(image);
+      },
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator(trailerLink) {
+        return validator.isURL(trailerLink);
+      },
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator(thumbnail) {
+        return validator.isURL(thumbnail);
+      },
+    },
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    req: 'user',
+    required: true,
+  },
+  movieId: {
+    type: String,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = model('movie', movieSchema);

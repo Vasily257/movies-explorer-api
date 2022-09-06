7d99f1f8b8b95494bdecc1a08f8db3db20fb3e7e
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-09-06 12:35:43 +0400

    feat: add winston logger and global logger

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 const helmet = require('helmet');
+const { requestLogger, errorLogger } = require('./src/middlewares/logger');
 
 const { rateOptions, corsOptions } = require('./src/utils/constants');
 
@@ -17,6 +18,9 @@ const app = express();
 app.use(rateLimit(rateOptions));
 app.use(cors(corsOptions));
 app.use(helmet());
+app.use(requestLogger);
+
+app.use(errorLogger);
 
 async function startServer() {
   await mongoose.connect('mongodb://localhost:27017/mestodb');
@@ -24,3 +28,10 @@ async function startServer() {
 }
 
 startServer();
+
+process.on('uncaughtException', (err, origin) => {
+  // eslint-disable-next-line no-console
+  console.log(
+    `${origin} ${err.name} c текстом ${err.message} не была обработана. Обратите внимание!`,
+  );
+});
